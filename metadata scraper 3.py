e69503e401450b5a42db09791115a37e28963c9a
mkywall <mkw5ck@virginia.edu>
2017-11-20 12:59:44 -0500

    GEO Metadata Scraper
    Takes in a list of series IDs, scrapes GEO for all associated samples, scrapes GEO for metadata from each sample.
    
    Future:  create functions to search for metadata features and organize information into pandas dataframe

---
+++ metadata scraper 3.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 20 11:36:57 2017
+
+@author: mkw5c
+"""
+
+import requests
+from bs4 import BeautifulSoup
+import re
+import os
+import numpy as np
+import pandas as pd
+
+
+def getgeo(series_ids):
+    GSM_ids = []
+    Download_manually = []
+    for id in series_ids: 
+        #download the page for the series
+        page = requests.get("https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=" + id)
+        #check if the page downloaded correctly
+        print(id + " Status: " + str(page.status_code))
+    
+        #create an instance of the beautiful soup class
+        soup = BeautifulSoup(page.content, 'html.parser')
+    
+        #find all of the "a" tags, get text from the a tags that have links to samples
+        a_tags = soup.find_all('a')
+        for a in a_tags: 
+            if a.get_text().startswith("GSM"):
+                GSM_ids.append(a.get_text())
+                if a.get_text() == 'You can also download a list of all accessions here':
+                    print(id + ": Too Many Samples: download a list of all accessions from GEO")
+                    Download_manually.append(id)
+    print("\n" + "Need to Download: " + str(Download_manually))
+    return(GSM_ids)
+
+def getsamplesmanually(file_path, sample_list):
+    files = []
+    for file in os.listdir(file_path):
+        if file.endswith(".txt"):
+            files.append(os.path.join(file_path, file))
+    for file in files: 
+        hand = open(file)
+        for line in hand: 
+            if line.startswith("!Series_sample_id"): 
+                sample_list.append(line[20:30])
+
+    for item in sample_list: 
+        item.strip()
+        if item.startswith("G") == False: 
+            print("fix" + item)
+    return(sample_list)
+        
+        
+def getsamplemetadata(sample_ids):
+    i = 0       
+    GSM_dict = {} 
+    for id in sample_ids:
+        i = i +1
+        if i%100 == 0: 
+            print(i)
+        page = requests.get("https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=" + id)
+        soup = BeautifulSoup(page.content, 'html.parser')  
+        td_tags = soup.find_all("td")
+        
+        sample_info = str()
+        for td in td_tags:
+            sample_info+= str(td.get_text())
+        GSM_dict.update({id: sample_info})
+    return(GSM_dict)
+
+###################################################################################################
+
+series_ids = ["GSE98563"] 
+
+#use the getgeo function to get the sample IDs for each series. 
+sample_ids = getgeo(series_ids)
+
+#get sample ids from manually downloaded files
+sample_ids = getsamplesmanually("C:/Users/mkw5c/documents/Neuro Capstone/GEO sample lists", sample_ids)
+
+#use the getsamplemetadata function to scrape the metadata for each sample from the GEO page
+#returns a dictionary with the sample id as the key and the metadata as a string value
+GSM_dictionary = getsamplemetadata(sample_ids)
+
+#create an empty dataframe to store the metadata in
+columns = ["exptype", "organism", "mouseline", "cellline", "strain", "organ", "source",\
+           "tissue", "selectionmarker", "celltype", "treatment", "age", "devstage", "gender"]
+index = sample_ids
+GSM_df = pd.DataFrame(index = index, columns = columns) 
+GSM_df= GSM_df.fillna(0) 
+
+#################################################################################################
+
+#create functions to search for metadata categories
+def findExpType(key, dictionary, df):
+    for line in dictionary[key]:
+        if re.search('Sample type', line):
+            return(line)
+        else: 
+            return("Not Found")
+
+for index in GSM_df.index.values: 
+    GSM_df.loc[index, "exptype"] = findExpType(index, GSM_dictionary, GSM_df)
+print(GSM_df["exptype"])
+    
+
+
+
+
+
