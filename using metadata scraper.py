0e3b17279187d1f02117d3e8edb82903b3fa8995
mkywall <mkw5ck@virginia.edu>
2017-12-06 18:23:42 -0500

    separate files for functions, scraping, and searching

--- using metadata scraper.py
+++ using metadata scraper.py
@@ -47,73 +47,9 @@ GSM_dictionary.update(getsamplemetadata(sample_ids[15000:16000]))
 GSM_dictionary.update(getsamplemetadata(sample_ids[16000:16565]))
 
 
-#create an empty dataframe to store the metadata in
-columns = ["series", "exptype", "organism", "mouseline", "cellline", "strain", "organ", "source",\
-           "selectionmarker", "celltype", "treatment", "age", "devstage", "sex"]
-index = sample_ids
-GSM_df = pd.DataFrame(index = index, columns = columns) 
 
-n = 0
-for sample in sample_ids: 
-    if sample in GSM_dictionary.keys():
-        n +=1
-    else:
-        print(sample + "\n")
-print(n)
 
-##use the functions
-for index in GSM_df.index.values: 
-    GSM_df.loc[index, "series"] = findSeries(index, GSM_dictionary, GSM_df)
-    
-for index in GSM_df.index.values: 
-    GSM_df.loc[index, "exptype"] = findExpType(index, GSM_dictionary, GSM_df)
 
-for index in GSM_df.index.values: 
-    GSM_df.loc[index, "organism"] = findOrganism(index, GSM_dictionary, GSM_df)
-
-for index in GSM_df.index.values: 
-    GSM_df.loc[index, "mouseline"] = findMouseline(index, GSM_dictionary, GSM_df)
-
-for index in GSM_df.index.values: 
-    GSM_df.loc[index, "cellline"] = findCellline(index, GSM_dictionary, GSM_df)
-    
-for index in GSM_df.index.values: 
-    GSM_df.loc[index, "strain"] = findStrain(index, GSM_dictionary, GSM_df)  
-
-#for index in GSM_df.index.values: 
-   # GSM_df.loc[index, "organ"] = findOrgan(index, GSM_dictionary, GSM_df)  
-
-for index in GSM_df.index.values: 
-    GSM_df.loc[index, "source"] = findSource_Tissue(index, GSM_dictionary, GSM_df) 
-
-for index in GSM_df.index.values: 
-   GSM_df.loc[index, "selection marker"] = findSelectionMarker(index, GSM_dictionary, GSM_df)
-
-for index in GSM_df.index.values: 
-    GSM_df.loc[index, "celltype"] = findCelltype(index, GSM_dictionary, GSM_df)
-    
-for index in GSM_df.index.values: 
-    GSM_df.loc[index, "treatment"] = findTreatment(index, GSM_dictionary, GSM_df)
-    
-for index in GSM_df.index.values: 
-   GSM_df.loc[index, "age"] = findAge(index, GSM_dictionary, GSM_df)
-
-for index in GSM_df.index.values: 
-    GSM_df.loc[index, "devstage"] = findDevelopment(index, GSM_dictionary, GSM_df)
-
-for index in GSM_df.index.values: 
-    GSM_df.loc[index, "sex"] = findSex(index, GSM_dictionary, GSM_df)
-
-########### CHECK FOR NA's ###############################################################
-
-keep_going = "yes"
-while keep_going != 'no':
-    column_to_check = input("Enter the name of the column you want to check for completion: ")
-    print(GSM_df[column_to_check].value_counts())
-    print(GSM_df.loc[GSM_df[column_to_check] == "Not Found", column_to_check])
-    print(GSM_df.loc[GSM_df[column_to_check] == "Not Found", "series"])
-    keep_going = input("Would you like to check another column? ").lower()
-    
 
 
 
