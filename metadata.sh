0516b1e238b493fd0838d9dbe5acc43b12064618
Cait Dreisbach <cnd2y@virginia.edu>
2017-12-13 16:39:06 -0500

    Update metadata.sh

--- metadata.sh
+++ metadata.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #Functions that support the scraper
-python GEO_metadata_scraper_functions.py
+python Metadata_scraper/GEO_metadata_scraper_functions.py
 
 #Pulling specific GEO series IDs for which we are interested in pulling the metadata
-python using_metadata_scraper.py
+python Metadata_scraper/using_metadata_scraper.py
 
 #Execution of the search functions on our dataframe and searching for missing values
-python using_the_search_functions.py
+python Metadata_scraper/using_the_search_functions.py
