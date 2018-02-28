907f4097f2e06a9b0a2ee4115fcd0d09f371367c
mkywall <mkw5ck@virginia.edu>
2018-02-28 18:03:58 -0500

    tfidf by brain region

---
+++ lun_cortex_tfidf.R
@@ -0,0 +1,86 @@
+
+
+cortex<- read.csv("cortex_df.csv")
+rownames(cortex)<- cortex[,1]
+cortex[,1]<- NULL
+
+#----- remove mitochondrial and spike in data
+cortex_red<- cortex[1:1312, c(1:19803, 19895)]
+
+#----- libraries
+library(readr) 
+library(tm)
+library(tidytext) 
+library(plyr) 
+library(dplyr) 
+library(ggplot2)
+
+#------ reshape the dataframe 
+cortex_reshape<- cbind(cortex$cluster_mem, stack(cortex[1:19803]))
+cortex_reshape[1:5, ]
+
+#------ rename the columns
+colnames(cortex_reshape)<- c("cluster", "expression", "gene")
+
+#------ check data types and for missing values
+str(cortex_reshape)
+sum(is.na(cortex_reshape["expression"]))
+
+#------ verify that there are 12 clusters
+unique(cortex_reshape$cluster)
+
+#------ group by cluster and determine the total and median expression 
+#       levels for each gene
+cortex_reshape$gene<- as.character(cortex_reshape$gene)
+cortex_reshape$expression<- as.numeric(cortex_reshape$expression)
+cortex_reshape$cluster<- as.character(cortex_reshape$cluster)
+
+cortex_grouped<- do.call(data.frame, aggregate(cortex_reshape$expression, 
+                                              by=list(cortex_reshape$cluster, cortex_reshape$gene), 
+                                              FUN=function(x) c(med.exp = median(x),avg.exp = mean(x), total.exp = sum(x))))
+
+#----- make a copy
+cortex_copy<- cortex_grouped
+cortex_copy[1:5, 1:5]
+
+#----- rename the columns
+colnames(cortex_copy)<- c("cluster", "gene", "med.exp", "avg.exp", "total.exp")
+
+#----- delete rows where the median expression is equal to zero
+cortex_copy<- cortex_copy[which(cortex_copy$med.exp >0), ]
+
+#----- bind the tfidf matrix using median as the threshold
+cortex_tfidf<- bind_tf_idf(tbl = cortex_copy, term = gene, document = cluster,  n = med.exp)
+
+#----- make a dataset to plot the top 15 genes by TF-IDF value
+plot_cortex <- cortex_tfidf %>%  
+  dplyr::arrange(desc(tf_idf))  %>%                                          
+  dplyr::mutate(gene = factor(gene, levels = rev(unique(gene)))) %>%           
+  dplyr::mutate(cluster = factor(cluster, levels = rev(unique(cluster))))
+
+to_print<- plot_cortex%>%
+           group_by(cluster) %>%    
+           top_n(50, tf_idf) %>%
+           ungroup() %>%
+           dplyr::mutate(gene = reorder(gene, tf_idf))
+
+to_print%>%
+    ggplot(aes(gene, tf_idf, fill = cluster)) +                 
+    geom_col(show.legend = FALSE) +
+    labs(x = NULL, y = "TFIDF score") +
+    facet_wrap(~cluster, ncol = 4, scales = "free") +
+    coord_flip()
+    
+print_me<- arrange(to_print, cluster, desc(tf_idf))
+
+save_the_genes<- function(df, clust_count){
+  for (i in 1:clust_count){
+    clust_genes<- df[which(df$cluster == i),c(2, 8)]
+    file = paste0("cortex_cluster_genes", i, ".rnk")
+    write.table(clust_genes, file, sep = "\t", row.names = F, col.names = F)}}
+
+save_the_genes(print_me, 12)
+
+
+
+
---
+++ lun_hippocampus_tfidf.R
@@ -0,0 +1,82 @@
+
+
+hippo<- read.csv("hippocampus_df.csv")
+rownames(hippo)<- hippo[,1]
+hippo[,1]<- NULL
+
+#----- remove mitochondrial and spike in data
+hippo_red<- hippo[1:1312, c(1:19803, 19895)]
+
+#----- libraries
+library(readr) 
+library(tm)
+library(tidytext) 
+library(plyr) 
+library(dplyr) 
+library(ggplot2)
+
+#------ reshape the dataframe 
+hippo_reshape<- cbind(hippo$cluster_mem, stack(hippo[1:19803]))
+hippo_reshape[1:5, ]
+
+#------ rename the columns
+colnames(hippo_reshape)<- c("cluster", "expression", "gene")
+
+#------ check data types and for missing values
+str(hippo_reshape)
+sum(is.na(hippo_reshape["expression"]))
+
+#------ verify that there are 16 clusters
+unique(hippo_reshape$cluster)
+
+#------ group by cluster and determine the total and median expression 
+#       levels for each gene
+hippo_reshape$gene<- as.character(hippo_reshape$gene)
+hippo_reshape$expression<- as.numeric(hippo_reshape$expression)
+hippo_reshape$cluster<- as.character(hippo_reshape$cluster)
+
+hippo_grouped<- do.call(data.frame, aggregate(hippo_reshape$expression, 
+                        by=list(hippo_reshape$cluster, hippo_reshape$gene), 
+                        FUN=function(x) c(med.exp = median(x),avg.exp = mean(x), total.exp = sum(x))))
+
+#----- make a copy
+hippo_copy<- hippo_grouped
+hippo_copy[1:5, 1:5]
+
+#----- rename the columns
+colnames(hippo_copy)<- c("cluster", "gene", "med.exp", "avg.exp", "total.exp")
+
+#----- delete rows where the median expression is equal to zero
+hippo_copy<- hippo_copy[which(hippo_copy$med.exp >0), ]
+
+#----- bind the tfidf matrix using median as the threshold
+hippo_tfidf<- bind_tf_idf(tbl = hippo_copy, term = gene, document = cluster,  n = med.exp)
+
+#----- make a dataset to plot the top 15 genes by TF-IDF value
+plot_hippo <- hippo_tfidf %>%  
+  dplyr::arrange(desc(tf_idf))  %>%                                          
+  dplyr::mutate(gene = factor(gene, levels = rev(unique(gene)))) %>%           
+  dplyr::mutate(cluster = factor(cluster, levels = rev(unique(cluster))))
+
+to_print<- plot_hippo%>%
+  group_by(cluster) %>%    
+  top_n(50, tf_idf) %>%
+  ungroup() %>%
+  dplyr::mutate(gene = reorder(gene, tf_idf))
+
+to_print%>%
+  ggplot(aes(gene, tf_idf, fill = cluster)) +                 
+  geom_col(show.legend = FALSE) +
+  labs(x = NULL, y = "TFIDF score") +
+  facet_wrap(~cluster, ncol = 4, scales = "free") +
+  coord_flip()
+
+print_me<- arrange(to_print, cluster, desc(tf_idf))
+
+save_the_genes<- function(df, clust_count){
+  for (i in 1:clust_count){
+    clust_genes<- df[which(df$cluster == i),c(2,8)]
+    file = paste0("hippo_cluster_genes", i, ".rnk")
+    write.table(clust_genes, file, sep = "\t", row.names = F, col.names = F)}}
+
+save_the_genes(print_me, 16)
